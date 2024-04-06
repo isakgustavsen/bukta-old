6b7ee76ef85e4114e6ce560303bb594e5b6ca574
Isak Wang Gustavsen <69854945+isakgustavsen@users.noreply.github.com>
2024-04-06 22:12:42 +0200

    Adding Kinde env to nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ["@nuxthub/core", "@nuxt/ui-pro"],
+  extends: ['@nuxthub/core', '@nuxt/ui-pro'],
   devtools: {
     enabled: true,
 
@@ -14,8 +14,16 @@ export default defineNuxtConfig({
       openAPI: true,
     },
   },
-  modules: ["@nuxt/ui", "@nuxtjs/kinde", "@nuxtjs/sanity"],
+  modules: ['@nuxt/ui', '@nuxtjs/kinde', '@nuxtjs/sanity'],
   sanity: {
-    projectId: "azn1k8lx",
+    projectId: 'azn1k8lx',
   },
-});
+  kinde: {
+    authDomain: process.env.NUXT_KINDE_AUTH_DOMAIN, 
+    clientId: process. env.NUXT_KINDE_CLIENT_ID, 
+    clientSecret: process.env.NUXT_KINDE_CLIENT_SECRET, 
+    redirectURL: process.env.NUXT_KINDE_REDIRECT_URL, 
+    logoutRedirecturL: process.env.NUXT_KINDE_LOGOUT_REDIRECT_URL,
+    postLoginRedirecturl: process.env.NUXT_KINDE_POST_LOGIN_REDIRECT_URL,
+  }
+})
