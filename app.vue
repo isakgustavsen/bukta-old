28c4bedb77d8570b4eec975c4cfcbdd4c5aefc1c
Isak Wang Gustavsen <69854945+isakgustavsen@users.noreply.github.com>
2024-04-07 02:05:26 +0200

    Update

--- app.vue
+++ app.vue
@@ -1,60 +1,39 @@
 <script setup lang="ts">
-// const client = useKindeClient()
+const client = useKindeClient()
 
 const links = ref([])
 
-// const {data: isAdmin} = await useAsyncData(async () => {
-//   return (await client?.getPermission('admin')) ?? {}
-// })
-
-
-
-// if(isAdmin?.value && isAdmin?.value.isGranted){
-const query = groq`*[_type == 'homePage']{
-  'label': title,
-  'children': *[_type == 'contentPage' && references(^._id)]|order(title asc){
+const {data: isAdmin} = await useAsyncData(async () => {
+  return (await client?.getPermission('admin')) ?? {}
+})
+if(isAdmin.value && isAdmin.value.isGranted){
+  const query = groq`*[_type == 'homePage']{
     'label': title,
-    'to': "/" + slug.current,
     'children': *[_type == 'contentPage' && references(^._id)]|order(title asc){
       'label': title,
-      'to': "/" + slug.current}
-  }
-}`
-
-const { data: adminLinks } = await useSanityQuery(query)
-links.value = adminLinks
-// }
-// else{
-//   const query = groq`*[_type == 'homePage' && _id == '0b6bd09e-c564-49b1-bfe7-fd5701b11e24']{
-//     'label': title,
-//     'children': *[_type == 'contentPage' && references(^._id)]|order(title asc){
-//       'label': title,
-//       'to': "/" + slug.current,
-//       'children': *[_type == 'contentPage' && references(^._id)]|order(title asc){
-//         'label': title,
-//         'to': "/" + slug.current}
-//     }
-//   }`
-
-//   let { data: linkArray } = await useSanityQuery(query)
-//   links.value = linkArray
-// }
-
-// const groups = [{
-//   key: 'users',
-//   label: q => q && `Users matching “${q}”...`,
-//   search: async (q) => {
-//     const query = groq`*[_type == "contentPage"]|score(pt::text(content) match $text){_score, title}`
-//     if (!q) {
-//       return []
-//     }
-
-//     const pages = await $fetch<any[]>('https://jsonplaceholder.typicode.com/users', { params: { q } })
-
-//     return pages.filter((page) => page.score > 0)
-//   }
-// }]
-
+      'to': "/" + slug.current,
+      'children': *[_type == 'contentPage' && references(^._id)]|order(title asc){
+        'label': title,
+        'to': "/" + slug.current}
+    }
+  }`
+  const { data: adminLinks } = await useSanityQuery(query)
+  links.value = adminLinks
+}
+else{
+  const query = groq`*[_type == 'homePage' && _id == '0b6bd09e-c564-49b1-bfe7-fd5701b11e24']{
+    'label': title,
+    'children': *[_type == 'contentPage' && references(^._id)]|order(title asc){
+      'label': title,
+      'to': "/" + slug.current,
+      'children': *[_type == 'contentPage' && references(^._id)]|order(title asc){
+        'label': title,
+        'to': "/" + slug.current}
+    }
+  }`
+  let { data: linkArray } = await useSanityQuery(query)
+  links.value = linkArray
+}
 </script>
 
 <template>
