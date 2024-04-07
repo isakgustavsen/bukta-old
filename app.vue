421a324e7ca15dd0f72b4cad79f7c8f0f6fac704
Isak Wang Gustavsen <69854945+isakgustavsen@users.noreply.github.com>
2024-04-07 02:42:55 +0200

    Fixed error

--- app.vue
+++ app.vue
@@ -1,15 +1,41 @@
 <script setup lang="ts">
-const query = groq`*[_type == 'homePage']{
-  'label': title,
-  'children': *[_type == 'contentPage' && references(^._id)]|order(title asc){
+
+const client = useKindeClient()
+const links = ref([])
+
+const {data: isAdmin} = await useAsyncData(async () => {
+  return (await client?.getPermission('admin')) ?? {};
+})
+
+if(isAdmin?.value?.isGranted){
+  const query = groq`*[_type == 'homePage']{
     'label': title,
-    'to': "/" + slug.current,
     'children': *[_type == 'contentPage' && references(^._id)]|order(title asc){
       'label': title,
-      'to': "/" + slug.current}
-  }
-}`
-const { data: links } = await useSanityQuery(query)
+      'to': "/" + slug.current,
+      'children': *[_type == 'contentPage' && references(^._id)]|order(title asc){
+        'label': title,
+        'to': "/" + slug.current}
+    }
+  }`
+  const { data } = await useSanityQuery(query)
+  links.value = data
+}
+else{
+  const query = groq`*[_type == 'homePage' && _id == '0b6bd09e-c564-49b1-bfe7-fd5701b11e24']{
+    'label': title,
+    'children': *[_type == 'contentPage' && references(^._id)]|order(title asc){
+      'label': title,
+      'to': "/" + slug.current,
+      'children': *[_type == 'contentPage' && references(^._id)]|order(title asc){
+        'label': title,
+        'to': "/" + slug.current}
+    }
+  }`
+  const { data } = await useSanityQuery(query)
+  links.value = data
+}
+
 </script>
 
 <template>
@@ -22,9 +48,11 @@ const { data: links } = await useSanityQuery(query)
           <UDashboardSearchButton />
         </template>
 
-        <UDashboardSidebarLinks :links="links[0].children" />
-        <UDivider label="Admin" />
-        <UDashboardSidebarLinks :links="links[1].children" />
+        <UDashboardSidebarLinks :links="links.value[0].children" />
+        <div v-if="isAdmin?.isGranted">
+          <UDivider label="Admin" />
+          <UDashboardSidebarLinks :links="links.value[1].children" />
+        </div>
 
         <UDivider />
 
